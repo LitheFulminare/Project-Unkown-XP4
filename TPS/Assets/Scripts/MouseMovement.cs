dab7acb73fd78afe8e5f7f407155273f06447f72
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2024-08-19 00:31:58 -0300

    movement and camera control

--- TPS/Assets/Scripts/MouseMovement.cs
+++ TPS/Assets/Scripts/MouseMovement.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class MouseMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float mouseSensitivity = 420f;
+
+    float xRotation = 0f;
+    float yRotation = 0f;
+
+    public float topClamp = -90f;
+    public float bottomClamp = 90f;
+    
     void Start()
     {
-        
+        // Locks the cursor and makes it invisible
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        // Gets mouse input
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        // Rotation around the X axis (up and down)
+        xRotation -= mouseY;
+
+        // Clamp the rotation
+        xRotation = Mathf.Clamp(xRotation, topClamp, bottomClamp);
+
+        // Rotation around the y axis (left and right);
+        yRotation += mouseX;
+
+        // Apply rotation to the transfom
+        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
     }
 }
