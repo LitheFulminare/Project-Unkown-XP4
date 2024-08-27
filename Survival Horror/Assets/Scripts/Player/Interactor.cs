d7d5857ada35edaab5a6ae7bccb0045bc72759ab
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2024-08-27 10:11:19 -0300

    comments

--- Survival Horror/Assets/Scripts/Player/Interactor.cs
+++ Survival Horror/Assets/Scripts/Player/Interactor.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Interactor : MonoBehaviour
 {
+    // follow this tutorial to make the interactor system
+    // https://www.youtube.com/watch?v=K06lVKiY-sY
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.F))
