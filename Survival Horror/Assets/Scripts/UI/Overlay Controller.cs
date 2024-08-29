52a2feef224f564f2d9c1e077797fe48dbed172b
LitheFulminare <164878888+LitheFulminare@users.noreply.github.com>
2024-08-29 10:47:11 -0300

    pickup overlay setup

---
+++ Survival Horror/Assets/Scripts/UI/Overlay Controller.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverlayController : MonoBehaviour
+{
+    public void Confirm()
+    {
+
+    }
+
+    public void Deny()
+    {
+
+    }
+}
