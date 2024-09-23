8715621a52dd849656daa3e235b8f317acea96c3
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2024-09-23 15:09:28 -0300

    comments

--- Survival Horror/Assets/Scripts/GameManager/CollectableController.cs
+++ Survival Horror/Assets/Scripts/GameManager/CollectableController.cs
@@ -7,10 +7,19 @@ using UnityEngine;
 public class CollectableController : MonoBehaviour
 {
     List<GameObject> collectables = new List<GameObject>();
+    private bool itemsAdded = false;
 
     private void Start()
     {
-        collectables.AddRange(GameObject.FindGameObjectsWithTag("Collectable"));
+        // find a way to save these. maybe add this to DontDestroyOnLoad? 
+        // i dont wanna make this static as every room should have independent lists
+        // but the data shouldnt be lost when switching scenes
+        if (!itemsAdded)
+        {
+            collectables.AddRange(GameObject.FindGameObjectsWithTag("Collectable"));
+            itemsAdded = true;
+        }
+        
         Debug.Log($"Elements in 'collectables: {collectables.Count}");
     }
 }
