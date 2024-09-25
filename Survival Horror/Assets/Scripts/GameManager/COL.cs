0d8d1d03670b915871aa0f138611198ec4831d56
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2024-09-24 21:28:35 -0300

    progress persistent collectables

--- Survival Horror/Assets/Scripts/GameManager/COL.cs
+++ Survival Horror/Assets/Scripts/GameManager/COL.cs
@@ -3,13 +3,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class COL : MonoBehaviour
+public class COL : MonoBehaviour // this will mainly keep track of destroyed items and destroy those on start
 {
     // this doesnt have any used right now, but who knows?
     [SerializeField] List<Collectable> collectables = new List<Collectable>();
 
     // keeps track of destroyed items
     public static List<string> destroyedItems = new List<string>();
+
+    // used on PlayerVars
+    // used to access the list of destroyed items belonging to the right scene
+    [SerializeField] int sceneIndex;
 
     private void Start()
     {
--- Survival Horror/Assets/Scripts/GameManager/PlayerVars.cs
+++ Survival Horror/Assets/Scripts/GameManager/PlayerVars.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerVars : MonoBehaviour
 {
@@ -17,8 +18,13 @@ public class PlayerVars : MonoBehaviour
 
     [SerializeField] InventoryController inventoryController;
 
+    // create a index for each scene and use it to create this and access this
+    public static List<List<GameObject>> destroyedItemsPerRoom = new List<List<GameObject>>();
+
     private void Start()
     {
+        //GetSceneDestroyedItems();
+
         // loads the inventory data if there is any
         // this is mainly used when switch scenes
         if (itemList == null && itemCount == null)
@@ -43,5 +49,10 @@ public class PlayerVars : MonoBehaviour
     public static void UpdateSpawnPosition(Vector3 newPos)
     {
         spawnPosition = newPos;
+    }
+
+    private void GetSceneDestroyedItems(List<GameObject> destroyedItems, int sceneIndex)
+    {
+        destroyedItemsPerRoom.Add(destroyedItems);
     }
 }
