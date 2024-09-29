0c06162ab29e612898dd252dfb83798658a8699c
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2024-09-29 13:25:51 -0300

    deactivate "EditorOnly" objects

--- Survival Horror/Assets/Scripts/Game Managers/Manager.cs
+++ Survival Horror/Assets/Scripts/Game Managers/Manager.cs
@@ -7,11 +7,15 @@ public class Manager : MonoBehaviour
     // keeps track of what the player is currently interacting with
     public static GameObject currentInteractionObj;
 
-    //public static Manager Instance;
+    private void Start()
+    {
+        List<GameObject> editorObjects = new List<GameObject>();
+        editorObjects.AddRange(GameObject.FindGameObjectsWithTag("EditorOnly"));
 
-    //private void Awake()
-    //{
-    //    Instance = this;
-    //    DontDestroyOnLoad(gameObject);
-    //}
+        foreach (GameObject obj in editorObjects)
+        {
+            Debug.Log($"Deactivating the object: {obj.name}");
+            obj.SetActive(false);
+        }
+    }
 }
