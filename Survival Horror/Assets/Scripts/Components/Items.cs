d03f85047ac0a65438e7746f9536d37e0d994e3d
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2024-10-04 18:25:45 -0300

    fixed items enum

--- Survival Horror/Assets/Scripts/Components/Items.cs
+++ Survival Horror/Assets/Scripts/Components/Items.cs
@@ -1,17 +1,25 @@
+// this enum stores the items
+// you need to assign int values to each item, it's ok if they're completely out of order
+// check this thread for more info:
+// https://discussions.unity.com/t/re-order-or-delete-enum-entries-will-it-causes-problems/910062
+
+// PLEASE UPDATE THE NUMBER BELOW
+// last item: 4
+
 public enum Items
 { 
     // empty space on the inventory
-    empty,
+    empty = 0,
 
     //ammo
-    pistolAmmo,
+    pistolAmmo = 1,
 
     // consumable
-    syringe,
+    syringe = 2,
 
     // weapon
-    pistol,
+    pistol = 3,
 
    // keys
-   keyDoor1
+   keyDoor1 = 4
 }
