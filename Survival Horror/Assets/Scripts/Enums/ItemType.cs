ac3837d8520146abfd14b0fa99c8291eb6efb57b
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2024-10-18 14:44:35 -0300

    introduced "ItemType" enum

---
+++ Survival Horror/Assets/Scripts/Enums/ItemType.cs
@@ -0,0 +1,6 @@
+public enum ItemType
+{
+    collectable = 1,
+    document = 2,
+    weapon = 3
+}
--- Survival Horror/Assets/Scripts/ScriptableObjects/CollectableSO.cs
+++ Survival Horror/Assets/Scripts/ScriptableObjects/CollectableSO.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Item", menuName = "ScriptableObject/Item")]
 public class CollectableSO : ScriptableObject
 {
+    // should adapt this so that documents are their own thing
+
     // general
+    [Header("General")]
     public string ingameName; // what appears on the prompt when the player tries to collect the item
     public string inventoryName; // what appears on the inventory
-    public bool isDocument; // when true, will go to the 'documents' tab instead of the regular inventory
+    public ItemType itemType; // wheter it's a document, a weapon, consumable, etc 
 
     // specific to collectable items, not documents
+    [Header("Collectable Only")]
     public int stack; // qtd gained when collected. 0 won't show any number on the inventory
     public Sprite iconImg;
     public GameObject inspectModel; // what appears when inspected
