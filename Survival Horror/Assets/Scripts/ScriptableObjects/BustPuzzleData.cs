c3084b0de1ecc9bbc50d1c6a41291757deb4aa29
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2024-10-20 18:45:46 -0300

    deleted unused bust puzzle stuff  from project

--- Survival Horror/Assets/Scripts/ScriptableObjects/BustPuzzleData.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "BustPuzzle", menuName = "SO Data/BustPuzzle")]
-public class BustPuzzleData : ScriptableObject
-{
-    public CollectableSO item;
-}
