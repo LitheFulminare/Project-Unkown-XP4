3cfb424458bfbb6df7fd244c9a7c0ed7c4cda12d
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2024-10-21 20:15:59 -0300

    added sprint when pressing shift

--- Survival Horror/Assets/Scripts/Player/TankMovement.cs
+++ Survival Horror/Assets/Scripts/Player/TankMovement.cs
@@ -14,23 +14,46 @@ public class TankMovement : MonoBehaviour
     public float speed = 250;
     public float turnSpeed = 180f;
 
+    private float sprintMultiplier = 1.7f;
+
+    private float sprintingSpeed;
+
     void Start()
     {
         controller = GetComponent<CharacterController>();
+
+        sprintingSpeed = speed * sprintMultiplier;
+        Debug.Log($"sprinting speed: {sprintingSpeed}");
     }
 
     void Update()
     {
         // handles the movement
         // currently this can be stopped by UI elements
+        
+        bool isSprinting = false;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            isSprinting = true;
+        }
+
         if (!PlayerVars.playerBlocked)
         {
+            float vel = speed;
+
+            if (isSprinting) vel = sprintingSpeed;
+
             Vector3 moveDir;
 
+            Debug.Log($"Current vel: {vel}");
+
             transform.Rotate(0, Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime, 0);
-            moveDir = transform.forward * Input.GetAxis("Vertical") * speed * Time.deltaTime;
+            //moveDir = transform.forward * Input.GetAxis("Vertical") * speed * Time.deltaTime;   
+
+            moveDir = Input.GetAxis("Vertical") * vel * Time.deltaTime * transform.forward; // apparently this has better performance
 
             controller.Move(moveDir * Time.deltaTime - Vector3.up * 0.1f);
-        }
+        }       
     }
 }
