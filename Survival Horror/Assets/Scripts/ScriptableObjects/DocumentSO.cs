09d45f973793697f88f3cb416b82a7eeb37450e2
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2024-10-22 11:03:48 -0300

    setup faded image on DocumentSO

--- Survival Horror/Assets/Scripts/ScriptableObjects/DocumentSO.cs
+++ Survival Horror/Assets/Scripts/ScriptableObjects/DocumentSO.cs
@@ -14,5 +14,7 @@ public class DocumentSO : ScriptableObject
 
     public Sprite backgroundImageAlt; // alternate version. Ex.: when the player uses the thermal goggles on the 'blank' paper they see another image
 
+    public Sprite inspectImage; // dark version of the backgroundImage when info is on top of the img, like text or something
+
     public DocumentType type; // whether it's a map, a tour guide or a record of some sort (the enum might be updated so new types will be added)
 }
