6f87406d79f9d862ca65ccc551315f30be956ee5
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2024-10-24 11:46:16 -0300

    show formatted text when inspecting

--- Survival Horror/Assets/Scripts/UI/Document Inspector.cs
+++ Survival Horror/Assets/Scripts/UI/Document Inspector.cs
@@ -3,16 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+public enum InspectorState
+{
+    Image = 0,
+    Text = 1,
+}
+
 public class DocumentInspector : MonoBehaviour
 {
     public delegate void SetDocument(DocumentSO documentSO);
     public static SetDocument setDocument;
 
     [SerializeField] Image documentImage;
+    [SerializeField] Text documentText;
 
     private DocumentSO _document;
 
     private float spawnTime;
+
+    //private InspectorState inspectorState = InspectorState.Image;
 
     private void Awake()
     {
@@ -34,11 +43,17 @@ public class DocumentInspector : MonoBehaviour
     {
         _document = document;
         documentImage.sprite = _document.backgroundImage;
+        documentText.text = "";
+    }
+
+    public void OnExit()
+    {
+        PlayerVars.BlockPlayer(true);
     }
 
     private void Update()
     {
-        if (Input.anyKey && Time.time > spawnTime + 1.5f)
+        if (Input.anyKey && Time.time > spawnTime + 1f)
         {
             ChangeImage();
         }
@@ -47,5 +62,32 @@ public class DocumentInspector : MonoBehaviour
     private void ChangeImage()
     {
         documentImage.color = Color.gray;
+        ShowText();
+
+        // this might be used later to know when to exit the inspection screen
+        //switch (inspectorState)
+        //{
+        //    case InspectorState.Image: documentImage.color = Color.gray; break;
+        //    case InspectorState.Text: ShowText(); break;
+        //}
+
+        //inspectorState = (inspectorState) + 1;
+
+    }
+
+    private void ShowText()
+    {
+        string formattedText = "";
+
+        for (int i = 0; i < _document.paragraphs.Length; i++)
+        {
+            if (i != 0)
+            {
+                formattedText += "\n\n" + _document.paragraphs[i];
+            }
+            else formattedText += _document.paragraphs[i];
+        }
+
+        documentText.text = formattedText;
     }
 }
