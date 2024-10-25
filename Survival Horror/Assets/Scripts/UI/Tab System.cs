18b2660f67e48161dbfd41be9ddd7a971dd1324f
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2024-10-25 20:24:50 -0300

    switch tabs and hide slots

--- Survival Horror/Assets/Scripts/UI/Tab System.cs
+++ Survival Horror/Assets/Scripts/UI/Tab System.cs
@@ -1,9 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime.Misc;
 using UnityEngine;
+using UnityEngine.UI;
 
-[Serializable]
 public enum Tabs
 {
     Inventory,
@@ -13,9 +14,42 @@ public enum Tabs
 
 public class TabSystem : MonoBehaviour
 {
+    [SerializeField] Sprite inventoryImage;
+    [SerializeField] Sprite documentsImage;
+    [SerializeField] Sprite mapImage;
+
+    [SerializeField] Image canvas;
+
+    [SerializeField] GameObject slots;
+
     private void ButtonClick(Tabs tabName)
     {
         Debug.Log($"Button Click was called with parameter {tabName.ToString()}");
+
+        if (tabName == Tabs.Inventory)
+        {
+            ShowInventory();
+            return;
+        }
+
+        slots.SetActive(false);
+
+        if (tabName == Tabs.Documents)
+        {
+            ShowDocuments();
+            return;
+        }
+    }
+
+    private void ShowInventory()
+    {
+        slots.SetActive(true);
+        canvas.sprite = inventoryImage;
+    }
+
+    private void ShowDocuments()
+    {
+        canvas.sprite = documentsImage;
     }
 
     public void InventoryButtonClick() => ButtonClick(Tabs.Inventory);
