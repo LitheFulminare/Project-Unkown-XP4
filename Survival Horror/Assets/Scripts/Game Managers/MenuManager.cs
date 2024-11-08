ed16bfbe7aa25097cd0304af48d58bd90206a34f
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2024-11-08 12:18:02 -0300

    setup MenuManager

---
+++ Survival Horror/Assets/Scripts/Game Managers/MenuManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] private string firstScene;
+
+    public void PlayButtonClick()
+    {
+
+    }
+}
